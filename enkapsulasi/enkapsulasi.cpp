abee37fa19bec2f455cf633f40c8c59b8394228e
hi-lufz1 <latifufauzi@gmail.com>
2023-05-29 20:27:19 +0700

    17

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -12,3 +12,12 @@ public:
 		return saklarNo[i];
 	}
 };
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "teras rumah");
+	lampuRumah.setSaklarNo(1, "ruang tamu");
+	lampuRumah.setSaklarNo(2, "kamar tidur");
+	lampuRumah.setSaklarNo(3, "dapur");
+
+}
