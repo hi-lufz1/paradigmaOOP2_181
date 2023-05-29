03330c7509b0bc00227d2506971ef96b170bdaf2
hi-lufz1 <latifufauzi@gmail.com>
2023-05-29 15:18:44 +0700

    7

---
+++ problemmultuwaris/problemmultiwaris.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+
+	}
+};
+
+class pekerja : public orang {
+public:
+
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja di buat\n" << endl;
+
+	}
+};
