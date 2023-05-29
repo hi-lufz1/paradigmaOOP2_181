ce92feff08aebb4f358e1b151d27ede603a83d38
hi-lufz1 <latifufauzi@gmail.com>
2023-05-29 14:49:51 +0700

    2

--- multipewarisan/multipewarisan.cpp
+++ multipewarisan/multipewarisan.cpp
@@ -10,9 +10,13 @@ public:
 		cout << "orang dibuat\n" << endl;
 	 }
 	~orang() {
-		cout<<"orang dihapus\n"<<endl
+		cout << "orang dihapus\n" << endl;
 	}
+};
 
+class manusia {
+public:
+	string jenisKelamin;
 
 
 };
