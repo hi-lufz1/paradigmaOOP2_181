636d25b41390978f12c2cf3318ff0df6c824c7c1
hi-lufz1 <latifufauzi@gmail.com>
2023-05-29 20:01:02 +0700

    15

--- nopewarisan/nopewarisan.cpp
+++ nopewarisan/nopewarisan.cpp
@@ -7,3 +7,17 @@ public:
 		cout << "hallo saya function dari base class";
 	}
 };
+
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "hallo saya function dari derived class";
+	}
+};
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
